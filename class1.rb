4bbde4757db3790ce772df0f31befae04b196197
Yuki-fujiif <84620909+Yuki-fujiif@users.noreply.github.com>
2022-05-30 19:47:37 +0900

    Add files via upload

---
+++ class1.rb
@@ -0,0 +1,15 @@
+p 1.class #=> Integer
+p 2.class
+p 100.class
+
+p "カフェラテ".class #=> String
+p "hello".class
+p "".class
+
+p [1,2,3].class #=> Array
+p ["コーヒー","カフェラテ"].class
+p [].class
+
+p 1.even?
+p 2.even?
+p 100.even?
---
+++ class4.rb
@@ -0,0 +1,9 @@
+p "カフェラテ"
+p [1,2,3]
+
+p String.new
+p String.new("カフェラテ")
+p Array.new
+p Array.new(2,"カフェラテ")
+
+#クラス.new #=>そのクラスのオブジェクト
