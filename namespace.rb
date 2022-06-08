913835c22606d1a4e97d2570a76b290021509b61
Yuki-fujiif <84620909+Yuki-fujiif@users.noreply.github.com>
2022-06-08 22:58:48 +0900

    Add files via upload

---
+++ namespace.rb
@@ -0,0 +1,20 @@
+module BecoCafe
+  class Coffee
+    def self.info
+      "深みと香りのコーヒー"
+    end
+  end
+end
+
+module MachuCafe
+  class Coffee
+    def self.info
+      "豊かな甘味のコーヒー"
+    end
+  end
+end
+
+puts BecoCafe::Coffee.info
+puts MachuCafe::Coffee.info
+
+#名前空間を分ける
