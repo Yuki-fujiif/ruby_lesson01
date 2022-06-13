e477c40e94f76b93b8455779dbd68e73a956fbc7
Yuki-fujiif <84620909+Yuki-fujiif@users.noreply.github.com>
2022-06-13 19:56:22 +0900

    Add files via upload

--- cat.rb
+++ cat.rb
@@ -1,1 +1,13 @@
+def cat(filename)
+  #ファイルの内容を表示する
+  File.open(filename) do |file| #1
+    file.each_line {|line| puts line } #2
+  end #3
+rescue SystemCallError => e #4
+  puts "--- 例外が発生しました ---"
+  puts "例外クラス: #{e.class}" #5
+  puts "例外メッセージ: #{e.message}" #6
+end
 
+filename = ARGV.first #7
+cat(filename)
